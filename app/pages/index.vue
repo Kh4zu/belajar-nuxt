bc1ec98fce08aeb790322d5eb974c21d8d5f5feb
Kh4zu <ajngrizki@gmail.com>
2025-11-14 10:44:59 +0700

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -78,10 +78,10 @@
         <span class="text-[#009879] font-semibold">madu hutan</span>.
       </p>
       <NuxtLink
-        to="/about"
+        to="/galeri"
         class="bg-[#009879] text-white px-6 sm:px-8 py-2.5 sm:py-3 rounded-full font-semibold shadow-md hover:bg-[#00BFA6] hover:scale-105 transition-all duration-300"
       >
-        Mengenali Lebih Lanjut
+        Jelajahi Produk Kami
       </NuxtLink>
     </section>
 
